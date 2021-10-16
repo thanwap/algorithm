3e71fda3d58da7f9a327af09f301a87e964e81d7
thanwap <dhunwa_p@hotmail.com>
2021-10-16 11:58:11 +0700

    Refactor binary search

--- src/searching/binary-search.js
+++ src/searching/binary-search.js
@@ -2,7 +2,7 @@ export const binarySearch = (arr, val) => {
   let i = 0,
     j = arr.length - 1;
 
-  while (i < j) {
+  while (i <= j) {
     const pivot = Math.ceil((j + i) / 2);
     const pivotVal = arr[pivot];
     if (val === pivotVal) {
@@ -12,10 +12,6 @@ export const binarySearch = (arr, val) => {
     } else {
       j = pivot - 1;
     }
-  }
-
-  if (i === j && val === arr[i]) {
-    return i;
   }
 
   return -1;
