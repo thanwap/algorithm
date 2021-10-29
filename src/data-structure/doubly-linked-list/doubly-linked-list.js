70da8508923712428c1f99c4628778448d8b522e
thanwap <dhunwa_p@hotmail.com>
2021-10-29 22:54:33 +0700

    Refactor push of DoublyLinkedList

--- src/data-structure/doubly-linked-list/doubly-linked-list.js
+++ src/data-structure/doubly-linked-list/doubly-linked-list.js
@@ -10,17 +10,12 @@ export class DoublyLinkdList {
     let newNode = new Node(val);
     if (this.length === 0) {
       this.head = newNode;
-      this.tail = this.head;
-    } else if (this.length === 1) {
-      this.tail = newNode;
-      this.head.next = this.tail;
-      this.tail.prev = this.head;
     } else {
       newNode.prev = this.tail;
       this.tail.next = newNode;
-      this.tail = newNode;
     }
 
+    this.tail = newNode;
     this.length++;
     return this;
   }
